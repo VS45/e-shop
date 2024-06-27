60c9604a71cdb9bff86ce41c63e8c1b59254c64c
vs45 <bringfirehjake88@gmail.com>
2024-06-27 11:10:46 +0100

    adding bootstrap themes

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const appRouter=require('./routes/app.routes')
 const path = require("path");
 const app = express();
 const PORT = 8080;
 app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(express.static(path.join(__dirname, "assets")));
+app.use(appRouter)
 app.listen(PORT, () => {
   console.log(`server runninng at port ${PORT}`);
 });
