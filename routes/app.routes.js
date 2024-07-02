8fb0e145481004f5800f1be3477b3a9eff737143
vs45 <bringfirehjake88@gmail.com>
2024-07-02 17:04:54 +0100

    navigation

--- routes/app.routes.js
+++ routes/app.routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { getHome } = require("../controller/homeController");
+const { getAbout } = require("../controller/aboutController");
 const router = express.Router();
 
 router.get("/", getHome);
+router.get("/about", getAbout);
 
 module.exports = router;
